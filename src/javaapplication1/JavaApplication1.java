2d884ceb5b08bda3abab634b77bd905f1dd9cebe
20024094 <20024094@10.5.144.235>
2023-09-12 11:30:12 +0100

    Test

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,15 @@
+package javaapplication1;
+
+/**
+ *
+ * @author 20024094
+ */
+public class JavaApplication1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Inverness");
+        System.out.println("Europe");
+    }
+    
+}
